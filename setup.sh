47e4cb29f0eeaa1146eb166d9752be34f2669333
ethanbbinghamton <143348036+ethanbbinghamton@users.noreply.github.com>
2025-04-12 18:20:12 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-sasfjoanjkjnkafjknanjakfnjikh
-mongod --dbpath /var/lib/mongo --logpath /var/log/mongodb/mongod.log --bind_ip 0.0.0.0 --fork
-mongosh --port 27017
-sudo make run
+dglksdnjisdfnkafdb;'fb'dvs
+fda
+zv
+bfdzv
+bzfx
+b
+'sfdb
+"zdf.b;'dfkm;bfdkmlbmklfbklmdfkmlbndfjk
