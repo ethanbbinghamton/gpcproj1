ce75bca34d9d9cf677ab2c1df71abbba26b07217
ethanbbinghamton <143348036+ethanbbinghamton@users.noreply.github.com>
2025-04-14 17:51:55 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mongod --dbpath /data/db --logpath /var/log/mongodb/mongod.log --bind_ip 0.0.0.0 --fork
-while true; mongosh --port 27017; done;
+while true; do mongosh --port 27017; done;
